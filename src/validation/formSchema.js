f53240ad4183f9a8172673d995ba84db6886c153
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2021-02-19 00:17:27 -0500

    updated schema

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,5 +1,5 @@
 import * as yup from 'yup'
 
 export default yup.object().shape({
-    btcUserAmount: yup.number('Must enter a number.').required('Enter how much BTC you have.')
+    btcUserAmount: yup.number().required('Enter how many Bitcoin.').typeError('How many Bitcoin do you own?')
 })
